17ef6e33c00bed04fc6bdb4ffed8d89c8a9de2c9
Andrei Bykov <dronbykov@gmail.com>
2017-04-13 12:02:16 +0300

    Add shared_prt for resolve ABA problem

--- main.cpp
+++ main.cpp
@@ -8,74 +8,76 @@ class lfqueue {
 private:
 
     struct node {
-        T data;
-        std::atomic<node *> next;
+        std::shared_ptr<T> data;
+        std::shared_ptr<node> next;
+
+        node(T const &_data) : data(std::make_shared<T>(_data)) {}
     };
 
-    std::atomic<node *> head{nullptr};
-    std::atomic<node *> tail{nullptr};
+    std::shared_ptr<node> head;
+    std::shared_ptr<node> tail;
 
     const unsigned MIN_DELAY = 1;
     const unsigned MAX_DELAY = 100;
     const unsigned FACTOR = 2;
 
 public:
-    lfqueue() {
-        node *empty_node = new node();
+    lfqueue(T data) {
+        std::shared_ptr<node> empty_node = std::make_shared<node>(data);
         empty_node->next = nullptr;
-        head.store(empty_node);
-        tail.store(empty_node);
+        head = empty_node;
+        tail = empty_node;
     }
 
     void push(T const &data) {
-        node *new_node = new node();
-        new_node->data = data;
+        std::shared_ptr<node> const new_node = std::make_shared<node>(data);
         new_node->next = nullptr;
 
-        node *cur_tail;
+        std::shared_ptr<node> cur_tail;
 
         int delay = MIN_DELAY;
 
         while (true) {
 
-            cur_tail = tail.load();
-            node *cur_next = cur_tail->next.load();
+            cur_tail = std::atomic_load(&tail);
+            std::shared_ptr<node> cur_next = std::atomic_load(&cur_tail->next);
 
-            if (cur_tail == tail.load()) {
+            if (cur_tail == std::atomic_load(&tail)) {
                 if (cur_next == nullptr) {
-                    if (cur_tail->next.compare_exchange_weak(cur_next, new_node)) {
+                    if (std::atomic_compare_exchange_weak(&cur_tail->next, &cur_next, new_node)) {
                         break;
                     }
                 } else {
-                    tail.compare_exchange_weak(cur_tail, cur_next);
+                    std::atomic_compare_exchange_weak(&tail, &cur_tail, cur_next);
                 }
             }
 
             std::this_thread::sleep_for(std::chrono::milliseconds(delay));
             delay = std::min(delay * FACTOR, MAX_DELAY);
         }
-        tail.compare_exchange_strong(cur_tail, new_node);
+        std::atomic_compare_exchange_strong(&tail, &cur_tail, new_node);
     }
 
-    bool pop(T &result) {
-        node *cur_head;
+    std::shared_ptr<T> pop() {
+        std::shared_ptr<T> result;
+        std::shared_ptr<node> cur_head;
 
         int delay = MIN_DELAY;
 
         while (true) {
-            cur_head = head.load();
-            node *cur_tail = tail.load();
-            node *cur_next = cur_head->next.load();
+            cur_head = std::atomic_load(&head);
+            std::shared_ptr<node> cur_tail = std::atomic_load(&tail);
+            std::shared_ptr<node> cur_next = std::atomic_load(&cur_head->next);
 
-            if (cur_head == head.load()) {
+            if (cur_head == std::atomic_load(&head)) {
                 if (head == tail) {
                     if (cur_next == nullptr) {
-                        return false;
+                        return std::shared_ptr<T>();
                     }
-                    tail.compare_exchange_weak(cur_tail, cur_next);
+                    std::atomic_compare_exchange_weak(&tail, &cur_tail, cur_next);
                 } else {
                     result = cur_next->data;
-                    if (head.compare_exchange_weak(cur_head, cur_next)) {
+                    if (std::atomic_compare_exchange_weak(&head, &cur_head, cur_next)) {
                         break;
                     }
                 }
@@ -83,24 +85,24 @@ public:
             std::this_thread::sleep_for(std::chrono::milliseconds(delay));
             delay = std::min(delay * FACTOR, MAX_DELAY);
         }
-        delete cur_head;
 
-        return true;
+        return result;
     }
 
 };
 
 int main() {
 
-    lfqueue<int> *lfqueue1 = new lfqueue<int>();
+    lfqueue<int> *lfqueue1 = new lfqueue<int>(1010);
     for (int i = 0; i < 10; i++)
         lfqueue1->push(i);
-    int res;
+
     for (int j = 0; j < 20; ++j) {
-        if (lfqueue1->pop(res)) {
-            std::cout << res << std::endl;
-        } else {
-            std::cout << "Queue is empty" << std::endl;
+        std::shared_ptr<int> result= lfqueue1->pop();
+        if (result != nullptr) {
+            std::cout << *result.get() << std::endl;
+        } else{
+            std::cout<<"Queue is empty"<<std::endl;
         }
     }
 
